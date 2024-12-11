1a7fc2d315a02017e968e68c145bccb2659fa3c3
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-12-11 13:03:03 -0500

    funciones js

---
+++ Funciones/convertirFaC.js
@@ -0,0 +1,5 @@
+function aCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+  
+  let value = aCelsius(77);
---
+++ Funciones/funciones.js
@@ -0,0 +1,11 @@
+function name(parameter1, parameter2, parameter3) {
+    // code to be executed
+  }
+
+// Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}

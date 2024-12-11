c1ae4577551d5b7671a9109092c0343edbeeec4d
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-12-11 12:58:59 -0500

    tipos de datos js

--- Tipo de datos/tiposDatos.js
+++ Tipo de datos/tiposDatos.js
@@ -1,5 +1,50 @@
 // TIPOS DE DATOS
 
-// String
+// Numbers:
+let length = 16;
+let weight = 7.5;
 
-var palabra = "Caracteres o texto"
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+
+// Object:
+const person = {firstName:"John", 
+    lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+// Using double quotes:
+let carName1 = "Volvo XC60";
+
+// Using single quotes:
+let carName2 = 'Volvo XC60';
+
+
+// Single quote inside double quotes:
+let answer1 = "It's alright";
+
+// Single quotes inside double quotes:
+let answer2 = "He is called 'Johnny'";
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+
+// With decimals:
+let x1 = 34.00;
+
+// Without decimals:
+let x2 = 34;
+
+let x = 5;
+let y = 5;
+let z = 6;
+(x == y)       // Returns true
+(x == z)       // Returns false
+
+

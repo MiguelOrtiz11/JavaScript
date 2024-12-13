2948ff199f3fc5b461eca23c2cf18304296d0ca3
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-12-12 22:40:55 -0500

    declaracion de funciones en js

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -2,8 +2,22 @@ function nombre(parametro1, parametro2, parametro3) {
   // Codigo a ejecutar
 }
 
-function myFunction(a, b) {
+// Forma 1
+function funcion1(a, b) {
   // Retorna la multiplicacion de a  y b
   return a * b;
 }
-console.log(myFunction(2, 3));
+console.log(funcion1(2, 3));
+
+// Forma 2
+function funcion2(a, b) {
+  console.log(a * b);
+}
+funcion2(2, 3);
+
+// Forma 3 - Declaracion de funcion tipo expresion
+let c = function (a, b) {
+  return a * b;
+};
+let resultado = c(2, 3);
+console.log(resultado);

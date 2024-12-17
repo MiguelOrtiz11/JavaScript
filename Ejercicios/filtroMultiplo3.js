114a79f4f5df9de54649d750100ac46b750de0e3
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-12-17 11:49:17 -0500

    filtrar multiplos de 3 en un arreglo de 100 numeros js

--- Ejercicios/filtroMultiplo3.js
+++ Ejercicios/filtroMultiplo3.js
@@ -2,22 +2,40 @@
 // en un arreglo que contenga los numeros del 1 al 100
 
 // Crear el arreglo con los numeros
-let numeros = [];
-for (let i = 1; i <= 100; i++) {
-  numeros.push(i);
-}
 
-console.log(numeros);
-
-function filtrarMultiplosDeTres(numeros) {
-  let numerosMultiplosTres = [];
-  for (let j = 1; j <= numeros.length; j++) {
-    if (numeros[j] % 3 === 0) {
-      numerosMultiplosTres.push(numeros[j]);
-    }
+function solucionMia() {
+  let numeros = [];
+  for (let i = 1; i <= 100; i++) {
+    numeros.push(i);
   }
-  return numerosMultiplosTres;
+  console.log(numeros);
+
+  function filtrarMultiplosDeTres(numeros) {
+    let numerosMultiplosTres = [];
+    for (let j = 1; j <= numeros.length; j++) {
+      if (numeros[j] % 4 === 0) {
+        numerosMultiplosTres.push(numeros[j]);
+      }
+    }
+    return numerosMultiplosTres;
+  }
+
+  let resultado = filtrarMultiplosDeTres(numeros);
+  console.log(resultado);
 }
 
-let resultado = filtrarMultiplosDeTres(numeros);
-console.log(resultado);
+function solucionYT() {
+  let nums = [];
+  let multiplos = [];
+  for (let i = 1; 1 <= 100; i++) {
+    nums.push(i);
+  }
+  console.log(`Este es el arreglo principal: $(nums)`);
+  nums.filter((el) => {
+    if (el % 3 === 0) multiplos.push(el);
+  });
+  console.log(multiplos);
+}
+
+solucionMia();
+solucionYT();
